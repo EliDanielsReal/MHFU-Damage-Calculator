1353a35af14c4db197d43ac1fb2ff60d886e08ef
Eli Daniels <elidanielsreal@gmail.com>
2024-03-19 00:35:00 +0200

    fixing bugs

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import typer
+from typing import Optional
+from typing_extensions import Annotated
 from calculate import calculate_raw
 from rich import print
 from styling import element_display, raw_display
 
 app = typer.Typer()
 
-@app.command()
-def raw(weapon_class: str, raw_attack: int, raw_element: int, element_type: str, sharpness: str = "GREEN"):
+
+
+@app.command("raw")
+def raw(weapon_class: str, raw_attack: int, raw_element: int, element_type: str, sharpness: Annotated[Optional[str], typer.Argument()] = "GREEN"):
 
     true_raw, true_element = calculate_raw(weapon_class, raw_attack, raw_element, sharpness)
 
-    display = raw_display(sharpness, true_raw) + " + " + element_display(element_type, true_element)
+    display = raw_display(sharpness, true_raw) + element_display(element_type, true_element)
 
     print(display)
 
--- styling.py
+++ styling.py
@@ -16,8 +16,8 @@ def raw_display(sharpness, raw_damage):
     return f'[italic green]({sharpness})[/italic green] [white]{int(raw_damage)} RAW [/white]'
 
 def element_display(element_type, element_value):
-    if element_type is None:
+    if element_type == "-":
         return ''
     
     colour = ELEMENT_COLOURS[element_type]
-    return f'[{colour}] {int(element_value)} {element_type} [/{colour}]'
+    return f' + [{colour}] {int(element_value)} {element_type} [/{colour}]'
